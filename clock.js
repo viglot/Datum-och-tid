6468cc0d74b2665ad7d742691743cebabbd5bc3a
Viglot <vigviglot36.hiltunen@gmail.com>
2021-08-10 10:54:33 +0300

    date

--- clock.js
+++ clock.js
@@ -5,19 +5,17 @@ var hours = currentTime.getHours();
 var minutes = currentTime.getMinutes();
 var seconds = currentTime.getSeconds();
 
-    
 
     var clockDiv = document.getElementById('clock');
+    
 
     clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
 
-    if (seconds < 10) {
-        seconds = "0" + seconds;
     }
-    }
-    
+
 
     displayTime();
+    
 
     setInterval(displayTime, 1000);
 
