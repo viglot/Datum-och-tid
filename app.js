0a51be1ea833959004c9a23fd0c3f122259e63dd
Viglot <vigviglot36.hiltunen@gmail.com>
2021-08-16 10:35:38 +0300

    funga

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const timeElement = document.getElementById("time");
 const dateElement = document.getElementById("date");
 
-setInterval(update, 100)
+update();
 
-function update(){
-    const date = new Date();
+setInterval(update, 100);
 
+function update() {
+  const date = new Date();
 
-timeElement.innerHTML =
-    pad(date.getHours(), 2) + ":" +
-  pad(date.getMinutes(), 2) + ":" +
- pad(date.getSeconds(), 2);
+  timeElement.innerHTML = 
+    pad(date.getHours(), 2) + ":" + 
+    pad(date.getMinutes(), 2) + ":" + 
+    pad(date.getSeconds(), 2);
 
-   dateElement.innerHTML =
-       date.getFullYear() + "-" +
-  pad(date.getMonth() + 1, 2) + "-" +
-   pad(date.getDate(), 2);
+  dateElement.innerHTML =
+    date.getFullYear() + "-" + 
+    pad(date.getMonth() + 1, 2) + "-" + 
+    pad(date.getDate(), 2); 
 }
 
-
-function pad(num, size){
-    num = num.toString
-    while(num.length < size) {
-        num = "0" + num;
-    }
-
-    return num;
+function pad(num, size) {
+  num = num.toString();
+  while(num.length < size) {
+    num = "0" + num;
+  }
+  return num;
 }
